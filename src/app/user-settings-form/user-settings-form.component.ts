013360fa6aed368e5dd258e82e1dfba32c3088f1
Leonardo Antonio Barbastefano <l.barbastefano@accenture.com>
2019-11-25 08:02:46 -0300

    Validating form and fields

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, LOCALE_ID } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -9,11 +10,11 @@ import { UserSettings } from '../data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   originalUserSettings: UserSettings = {
-    name: '',
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: 'some notes'
+    name: 'null',
+    emailOffers: null,
+    interfaceStyle: 'null',
+    subscriptionType: 'null',
+    notes: 'null'
   };
 
   //spread operator is the '...'
@@ -23,4 +24,15 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onBlur(field : NgModel){
+    console.log('in onBlur: ',  field.valid);
+   
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ',  form.valid);
+   }
+
 }
+
+
