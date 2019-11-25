bd45e459c1de728921b20f1c0500c8f10d61b0e6
Leonardo Antonio Barbastefano <l.barbastefano@accenture.com>
2019-11-25 19:02:49 -0300

    Create data Service

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -10,29 +13,49 @@ import { NgForm, NgModel } from '@angular/forms';
 export class UserSettingsFormComponent implements OnInit {
 
   originalUserSettings: UserSettings = {
-    name: 'null',
+    name: null,
     emailOffers: null,
-    interfaceStyle: 'null',
-    subscriptionType: 'null',
-    notes: 'null'
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   };
 
   //spread operator is the '...'
   userSettings : UserSettings = { ...this.originalUserSettings };
-  constructor() { }
+  postError = false;
+  postErrorMessage = '';
+  //subscriptionTypes =['one', 'two', 'three'];
+  subscriptionTypes: Observable<string[]>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
   }
 
   onBlur(field : NgModel){
     console.log('in onBlur: ',  field.valid);
-   
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log('error: ' , errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
   }
 
   onSubmit(form: NgForm) {
     console.log('in onSubmit: ',  form.valid);
+
+    if(form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log('success: ', result),
+        error => this.onHttpError(error)
+      );
+    }
+    else{
+      this.postError = true;
+      this.postErrorMessage = "Please fix above errors";
+    }
    }
 
 }
-
-
